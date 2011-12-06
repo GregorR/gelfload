fef02e4864bc3b7076b183b731d4e6a287e1cf6a
Gregor Richards <Richards@codu.org>
2011-12-06 13:02:11 -0500

    Addition of LD_LIBRARY_PATH and /lib/ld-* loading.
    --HG--
    branch : munge

--- src/findlibrary.c
+++ src/findlibrary.c
@@ -1,3 +1,5 @@
+#define _POSIX_SOURCE /* for strtok_r */
+
 #include <glob.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,11 +7,55 @@
 
 #include "findlibrary.h"
 
+char **ldLibraryPath = NULL;
+
 char *globLibrary(char *buf, const char *dir, const char *base);
 
 char *findLibrary(const char *metanm)
 {
     char *metanmd, *postlib, *filenm, *foundlib;
+    char *ldLibraryPathEnv, *saveptr;
+    int i, llpElems, maxLen = 16;
+
+    /* get our LD_LIBRARY_PATH if applicable */
+    if (ldLibraryPath == NULL) {
+        ldLibraryPathEnv = getenv("LD_LIBRARY_PATH");
+        if (ldLibraryPathEnv) {
+            /* figure out how many entries */
+            ldLibraryPathEnv = strdup(ldLibraryPathEnv);
+            if (ldLibraryPathEnv == NULL) {
+                perror("strdup");
+                exit(1);
+            }
+            llpElems = 1;
+            for (i = 0; ldLibraryPathEnv[i]; i++) {
+                if (ldLibraryPathEnv[i] == ':') llpElems++;
+            }
+
+            /* split it up */
+            ldLibraryPath = malloc((llpElems + 1) * sizeof(char*));
+            if (ldLibraryPathEnv == NULL) {
+                perror("malloc");
+                exit(1);
+            }
+            ldLibraryPath[0] = strtok_r(ldLibraryPathEnv, ":", &saveptr);
+            if (strlen(ldLibraryPath[0]) > maxLen) maxLen = strlen(ldLibraryPath[0]);
+            for (i = 1; ldLibraryPath[i] = strtok_r(NULL, ":", &saveptr); i++)
+                if (strlen(ldLibraryPath[i]) > maxLen) maxLen = strlen(ldLibraryPath[i]);
+
+        } else {
+            /* empty list */
+            ldLibraryPath = malloc(sizeof(char*));
+            if (ldLibraryPath == NULL) {
+                perror("malloc");
+                exit(1);
+            }
+            ldLibraryPath[0] = NULL;
+
+        }
+    }
+
+    maxLen += 32;
 
     metanmd = strdup(metanm);
     if (metanmd == NULL) {
@@ -25,7 +71,7 @@ char *findLibrary(const char *metanm)
     }
 
     /* make some space for it */
-    filenm = malloc(strlen(postlib) + 32);
+    filenm = malloc(strlen(postlib) + maxLen);
     if (filenm == NULL) {
         perror("malloc");
         exit(1);
@@ -33,17 +79,23 @@ char *findLibrary(const char *metanm)
 
     /* split it by dots */
     while (1) {
+        /* first try LD_LIBRARY_PATH */
+        for (i = 0; ldLibraryPath[i]; i++) {
+            if (foundlib = globLibrary(filenm, ldLibraryPath[i], postlib)) break;
+        }
+        if (ldLibraryPath[i]) break;
+
         /* first try /lib and /usr/lib */
-        if (foundlib = globLibrary(filenm, "/lib/", postlib)) break;
-        if (foundlib = globLibrary(filenm, "/usr/lib/", postlib)) break;
+        if (foundlib = globLibrary(filenm, "/lib", postlib)) break;
+        if (foundlib = globLibrary(filenm, "/usr/lib", postlib)) break;
 
         /* then with 32 or 64 */
         if (sizeof(size_t) == 8) {
-            if (foundlib = globLibrary(filenm, "/lib64/", postlib)) break;
-            if (foundlib = globLibrary(filenm, "/usr/lib64/", postlib)) break;
+            if (foundlib = globLibrary(filenm, "/lib64", postlib)) break;
+            if (foundlib = globLibrary(filenm, "/usr/lib64", postlib)) break;
         } else if (sizeof(size_t) == 4) {
-            if (foundlib = globLibrary(filenm, "/lib32/", postlib)) break;
-            if (foundlib = globLibrary(filenm, "/usr/lib32/", postlib)) break;
+            if (foundlib = globLibrary(filenm, "/lib32", postlib)) break;
+            if (foundlib = globLibrary(filenm, "/usr/lib32", postlib)) break;
         }
 
         /* of course that list isn't very portable */
@@ -56,7 +108,8 @@ char *findLibrary(const char *metanm)
 
     /* some hardwired alternates */
     if (foundlib == NULL) {
-        if (!strncmp(metanm, "libintl.so.", 11) ||
+        if (!strncmp(metanm, "ld", 2) ||
+            !strncmp(metanm, "libintl.so.", 11) ||
             !strncmp(metanm, "libiconv.so.", 12) ||
             !strncmp(metanm, "libsocket.so.", 13) ||
             !strncmp(metanm, "libgen.so.", 10)) {
@@ -78,7 +131,7 @@ char *globLibrary(char *buf, const char *dir, const char *base)
     glob_t pglob;
     int globr;
 
-    sprintf(buf, "%slib%s.*", dir, base);
+    sprintf(buf, "%s/lib%s.*", dir, base);
 
     /* now try to glob */
     globr = glob(buf, 0, NULL, &pglob);
