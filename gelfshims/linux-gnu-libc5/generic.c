629146626450d11b5c1262ee687b8789391c7feb
Gregor Richards <Richards@codu.org>
2011-12-06 19:41:23 -0500

    Updating linux-gnu-libc5 target.
    --HG--
    branch : munge

--- gelfshims/linux-gnu-libc5/generic.c
+++ gelfshims/linux-gnu-libc5/generic.c
@@ -1,6 +1,8 @@
+#include <setjmp.h>
 #include <stdio.h>
 #include "shim.h"
 
 void SHIM(__ctype_b)() {}
 void SHIM(__libc_init)() {}
 void SHIM(__setfpucw)() {}
+void SHIM(__setjmp)(jmp_buf env) { return setjmp(env); }
