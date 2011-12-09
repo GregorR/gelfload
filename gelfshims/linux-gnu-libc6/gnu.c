7ed5af37fe5cb907cbc294ff4258ccc3157620fc
Gregor Richards <Richards@codu.org>
2011-12-09 02:33:54 +0000

    Adding GNU __rawmemchr.
    --HG--
    branch : munge

--- gelfshims/linux-gnu-libc6/gnu.c
+++ gelfshims/linux-gnu-libc6/gnu.c
@@ -7,3 +7,8 @@ void *SHIM(mempcpy)(void *a, const void *b, size_t c)
     memcpy(a, b, c);
     return (void *) ((char *) a + c);
 }
+
+void *SHIM(__rawmemchr)(const void *a, int b)
+{
+    return memchr(a, b, (size_t) -1);
+}
