8047fd44f6def11f322b429f822c5bea1a5d5221
Gregor Richards <Richards@codu.org>
2012-08-11 15:40:02 -0400

    Use whereAmI in gelfload-ld for argv[0]

--- src/elfload-ld.c
+++ src/elfload-ld.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "../config.h"
+#include "whereami.h"
 
 int main(int argc, char **argv)
 {
@@ -12,6 +13,16 @@ int main(int argc, char **argv)
     newargs = alloca((argc + 2) * sizeof(char*));
     memcpy(newargs + 1, argv, (argc + 1) * sizeof(char*));
     newargs[0] = PREFIX "/bin/gelfload";
+
+    /* make the program name absolute */
+    if (argv[0][0] != '/') {
+        char *dir, *fil;
+        if (whereAmI(argv[0], &dir, &fil)) {
+            char *newargv1 = alloca(strlen(dir) + strlen(fil) + 2);
+            sprintf(newargv1, "%s/%s", dir, fil);
+            newargs[1] = newargv1;
+        }
+    }
 
     /* then call */
     execv(newargs[0], newargs);
